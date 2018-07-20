a5050d3bff041aa6fdc82026a415ae55bba2086c
Micha Krapp <github@michakrapp.de>
2018-07-21 00:03:32 +0200

    basic install / uninstall functions

--- gp-sync-to-wp.php
+++ gp-sync-to-wp.php
@@ -11,3 +11,27 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  gp-sync-to-wp
 Domain Path:  /languages
 */
+
+/**
+ * Plugin - activation
+ */
+function gp_sync_to_wp_activation() {
+  error_log( 'Plugin GP sync to WP activated.' );
+}
+register_activation_hook( __FILE__, 'gp_sync_to_wp_activation' );
+
+/**
+ * Plugin - deactivation
+ */
+function gp_sync_to_wp_deactivation() {
+  error_log( 'Plugin GP sync to WP deactivated.' );
+}
+register_deactivation_hook( __FILE__, 'gp_sync_to_wp_deactivation' );
+
+/**
+ * Plugin - uninstall
+ */
+function gp_sync_to_wp_uninstall() {
+  error_log( 'Plugin GP sync to WP uninstalled.' );
+}
+register_uninstall_hook( __FILE__, 'gp_sync_to_wp_uninstall' );
