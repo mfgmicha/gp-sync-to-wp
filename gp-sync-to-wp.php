b8b893971a3bef8a911b06e739bc35d808fbb4af
Micha Krapp <github@michakrapp.de>
2018-07-20 23:14:59 +0200

    update plugin header

---
+++ gp-sync-to-wp.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  GlotPress Sync to WordPress
+Plugin URI:   https://michakrapp.com
+Description:  GlotPress Plugin to sync translations to the WordPress site it is installed on
+Version:      0.0.1
+Author:       Micha Krapp
+Author URI:   https://michakrapp.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  gp-sync-to-wp
+Domain Path:  /languages
+*/
